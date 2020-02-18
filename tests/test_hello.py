f327fddf3199735fc8f9db7e0282baf22f9e5301
Hembert Iregui <hembert.iregui@bitsamericas.com>
2020-02-18 16:26:32 -0500

    [ADD] update coverage

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,4 +5,4 @@ def test_says_world():
     assert hello.say_what() == 'world'
 
 def test_says_hello():
-    assert hello.hello(hello.say_what()) == 'hello world'
+    assert hello.hello(hello.say_what()) == 'Hello, world!'
